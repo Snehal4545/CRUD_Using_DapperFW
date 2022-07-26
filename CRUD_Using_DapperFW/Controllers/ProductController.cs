0125fa95e95cd83544892f16cad9418ffa07388c
Snehal <pachundkars6@gmail.com>
2022-07-26 10:37:34 +0530

    Add project files.

---
+++ CRUD_Using_DapperFW/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Using_DapperFW.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var model = ProductDAL.GetAllProducts();
+            return View(model);
+
+            
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+
+            try
+            {
+                int result = ProductDAL.AddProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                int result = ProductDAL.UpdateProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                int result = ProductDAL.DeleteProduct(id);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
+
